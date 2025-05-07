44824af55bd13f982065a2994b935de87a8bbffa
pohpuaykai <puaykaipoh+a@gmail.com>
2025-05-07 14:46:11 +0800

    UNTESTED: CODE_REFACTORING
    unit_test for new latex parser

---
+++ foundation/automat/parser/tests/latexparser/unittest.py
@@ -0,0 +1,118 @@
+import pprint
+
+from latexparser import Latexparser
+
+pp = pprint.PrettyPrinter(indent=4)
+
+#unit tests
+def test__remove_space(verbose=False):
+    #from https://en.wikipedia.org/wiki/Quaternionic_matrix
+    
+    equationStr = " a + bi + cj + dk =  \\begin{pmatrix} a+ bi&c+d i  \\\\-c+di &a  -bi \\\\ \\end{pmatrix}"
+    parser = Latexparser(equationStr, verbose=verbose)
+    parser._remove_all_spacing()
+    if verbose:
+        print(parser.equationStr)
+        
+
+def test__find_matrices(verbose=False):
+    #(a+bi+cj+dk)(a+bi+cj+dk)
+    
+    equationStr = "\\begin{pmatrix}(a^2-b^2-c^2-d^2)+i(2ab)&2ac+i(2ad)\\\\-2ac+i(2ad)&(a^2+b^2-c^2-d^2)-i(2ab)\\\\\\end{pmatrix}=\\begin{pmatrix}a+bi&c+di\\\\-c+di&a-bi\\\\\\end{pmatrix}\\begin{pmatrix}a+bi&c+di\\\\-c+di&a-bi\\\\\\end{pmatrix}"
+    parser = Latexparser(equationStr, verbose=verbose)
+    parser._remove_all_spacing()
+    parser._find_matrices()
+    expected = [
+    ((1, 16, '\\begin{pmatrix}'), (194, 207, '\\end{pmatrix}')), 
+    ((105, 120, '\\begin{pmatrix}'), (143, 156, '\\end{pmatrix}')), 
+    ((156, 171, '\\begin{pmatrix}'), (91, 104, '\\end{pmatrix}'))
+    ]
+    if verbose:
+        print(parser.matrices_pos_type)
+
+def test__find_infix(verbose=False): #TODO infix test for \\to;\\cross;\\cdot
+    #\forall k \in \Z^+ and \forall prime_number p, equationStr = 1 | -1 a multivalue function
+    equationStr = "(\\frac{p+1}{2})*((p-1)/2)^{(k-1)(p-1)/2})\\%p=p-1"
+    parser = Latexparser(equationStr, verbose=verbose)
+    parser._remove_all_spacing()
+    #parser._find_matrices()
+    parser._find_infix()
+    expected = [
+    (26, 27, '^'), 
+    (23, 24, '/'), 
+    (38, 39, '/'), 
+    (42, 44, '\\%'), 
+    (16, 17, '*'), 
+    (20, 21, '-'), 
+    (30, 31, '-'), 
+    (35, 36, '-'), 
+    (47, 48, '-'), 
+    (9, 10, '+'), 
+    (45, 46, '=')
+    ]
+    if verbose:
+        print(parser.infixs_pos_type) #TODO
+        
+def test__find_brackets(verbose=False):
+    if verbose:
+        print() # TODO
+        
+def test__find_backslash(verbose=False):
+    
+    equationStr = "\\lim_{\\theta\\to\\infty}{\\sum_{n=-\\theta}^{\\theta}{\\frac{1}{P}\\int_{P/2}^{-P/2} f(x)e^{-i2\\pi\\frac{n}{P}x}dx}(e^{i2\\pi\\frac{n}{P}x})}"
+    parser = Latexparser(equationStr, verbose=verbose)
+    parser._remove_all_spacing()
+    #parser._find_matrices()
+    #parser._find_infix()
+    parser._find_backslash()
+    expected = None
+    if verbose:
+        print() # TODO
+        
+
+def test__find_variables_or_numbers(verbose=False):
+    if verbose:
+        print() # TODO
+        
+def test__find_implicit_0(verbose=False):
+    if verbose:
+        print() # TODO
+        
+def test__find_implicit_multiply(verbose=False):
+    if verbose:
+        print() # TODO
+        
+def test__find_infixes_width(verbose=False):
+    if verbose:
+        print() # TODO
+        
+def test__match_child_to_parent_input(verbose=False):
+    if verbose:
+        print() # TODO
+        
+def test__format_to_pyStyledAST(verbose=False):
+    if verbose:
+        print() # TODO
+        
+def test__convert_to_schemeStyledAST(verbose=False):
+    if verbose:
+        print() # TODO
+        
+def test__get_statistics(verbose=False):
+    if verbose:
+        print() # TODO
+
+if __name__=='__main__':
+    #test__remove_space(True) # pass
+    test__find_matrices(True)
+    #test__find_infix(True)
+    #test__find_backslash(True)
+    #test__find_brackets(True)
+    #test__find_variables_or_numbers(True)
+    #test__find_implicit_0(True)
+    #test__find_implicit_multiply(True)
+    #test__find_infixes_width(True)
+    #test__match_child_to_parent_input(True)
+    #test__format_to_pyStyledAST(True)
+    #test__convert_to_schemeStyledAST(True)
+    #test__get_statistics(True)
